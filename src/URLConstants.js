e3c1a00d1160d72503ab90b0a40c07d57b5fe883
DavinShee <105922278+DavinShee@users.noreply.github.com>
2022-06-26 14:13:50 +0800

    Update URLConstants.js

--- src/URLConstants.js
+++ src/URLConstants.js
@@ -1,6 +1,6 @@
 export const databaseURLs = {
   BUYER:
-    'https://aad1-2406-3003-206f-4ac0-9443-285e-21be-610e.ap.ngrok.io' +
+    'https://cd84-116-15-168-211.ap.ngrok.io' +
     '/routes/buyer',
   URL2: 'google.com'
 };
