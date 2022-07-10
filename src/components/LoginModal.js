6523cca88c5bd126d6931deac618a686fd5a10be
DavinShee <105922278+DavinShee@users.noreply.github.com>
2022-07-10 11:13:38 +0800

    etc

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -46,7 +46,7 @@ const LoginModal = ({ showLoginModal, setLoginModal, handleShowSignup }) => {
         headers: requestHeader
       })
       .then((response) => {
-        // console.log('Success=======>', response);
+         console.log('Success=======>', response);
         localStorage.setItem(
           'user',
           JSON.stringify({ email: `${loginFormValues.email}` })
@@ -54,7 +54,7 @@ const LoginModal = ({ showLoginModal, setLoginModal, handleShowSignup }) => {
         window.location.reload(false);
       })
       .catch((error) => {
-        // console.log('Error=========>', error);
+         console.log('Error=========>', error);
         setAlertDetails({
           variant: 'danger',
           message: 'Invalid email address or password. Please try again.'
@@ -90,7 +90,7 @@ const LoginModal = ({ showLoginModal, setLoginModal, handleShowSignup }) => {
               name="password"
             />
           </FloatingLabel>
-          <Button variant className='login-login-button'>Login!</Button>
+          <Button variant className='login-login-button' type='submit'>Login!</Button>
         </Form>
         <br />
         Don't have an account?{' '}
