abe7d6c74745d17594ffea08befd0faa041fba2d
DavinShee <105922278+DavinShee@users.noreply.github.com>
2022-07-27 22:00:39 +0800

    pikachu

--- src/components/LoadingModal.js
+++ src/components/LoadingModal.js
@@ -11,7 +11,10 @@ const LoadingModal = ({ LoadingModal }) => {
           <h2>Please wait as we process your request.</h2>
         </div>
         <div className="bars-container">
-          <ReactLoading className="bars-loading" type="bars" color="black" />
+          {/* <ReactLoading className="bars-loading" type="bars" color="black" /> */}
+          <img
+            src={`https://i.pinimg.com/originals/66/89/dc/6689dc331be27e66349ce9a4d15ddff3.gif`}
+          />
         </div>
       </Modal.Body>
     </Modal>
